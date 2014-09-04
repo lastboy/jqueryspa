29529f14f532d40c7593a034fff53e7307c0cab6
arik <lewin.arik@gmail.com>
2014-09-04 20:33:28 +0300

    catjs project added

---
+++ cat-project/target/jqueryspa-test/js/controllers/app.js
@@ -0,0 +1,38 @@
+/*global define*/
+'use strict';
+
+/**
+ * The main controller for the our app
+ */
+
+define(['app', 'common/manager', 'services/data', "directives/scrollto"], function (app, manager, appdata, scrolldirective) {
+    
+    // App controller creation
+    app.controller('appController', ['appData', '$scope', "$interval", function(appData, $scope, $interval) {
+
+        // Initial selected section as empty
+        $scope.selectedSection = {name: "", data: undefined};
+        
+        // On meta-data retrieved setup the UI accordingly
+        appData.then(function(data) {
+            
+            // Set the data into scope
+            $scope.pages = data.all();
+
+            // Run the common script
+            var _interval;
+            
+            if (data) {
+                _interval = $interval(function(){
+                    if (manager.init($scope, scrolldirective, data)) {
+                        $interval.cancel(_interval);
+                    }
+                }, 200) ;
+            }
+
+        });
+
+      
+    }]);
+    
+});
